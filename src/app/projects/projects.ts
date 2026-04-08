31e26c6904f7dcd903ca5afcf6f11c1424284f61
crazyman222214 <schadejr617@gmail.com>
2026-04-08 17:42:31 -0600

    Adding More Projects

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { TypingComponent } from "../typing.component";
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [TypingComponent],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
@@ -16,4 +17,8 @@ export class Projects {
     this.router.navigate([path]);
   }
 
+  goToExternal(path: string) {
+    console.log('navigating to', path);
+    window.open(path, '_blank');
+  }
 }
